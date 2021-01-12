dec74936485eabae027351e27d14fe2df37c2827
Rosyid <rosyidgrobogan@gmail.com>
2021-01-12 21:46:14 +0700

    test: testAddTodoList()

--- src/test/service/TodoListServiceTest.java
+++ src/test/service/TodoListServiceTest.java
@@ -7,12 +7,25 @@ import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testShowTodoList();
+        //testShowTodoList();
+        testAddTodoList();
     }
 
     public static void testShowTodoList(){
         TodoListRepository todoListRepository = new TodoListRepositoryImpl();
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.showTodoList();
+    }
+
+    public static void testAddTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Olah Raga");
+        todoListService.addTodoList("Sarapan");
+        todoListService.addTodoList("Baca Koran");
+        todoListService.addTodoList("Persiapan Kerja");
 
         todoListService.showTodoList();
     }

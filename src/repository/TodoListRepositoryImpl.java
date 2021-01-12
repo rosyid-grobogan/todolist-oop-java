68629a98688f1190ce8b5e2b2516621eda8de7eb
Rosyid <rosyidgrobogan@gmail.com>
2021-01-12 21:14:17 +0700

    data layer of getAll()

---
+++ src/repository/TodoListRepositoryImpl.java
@@ -0,0 +1,23 @@
+package repository;
+
+import entity.TodoList;
+
+public class TodoListRepositoryImpl implements TodoListRepository{
+
+    private TodoList[] data = new TodoList[10];
+
+    @Override
+    public TodoList[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void add(TodoList todoList) {
+
+    }
+
+    @Override
+    public void remove(Integer number) {
+
+    }
+}

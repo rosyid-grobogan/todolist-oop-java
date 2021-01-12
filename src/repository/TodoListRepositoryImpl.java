dd7a420b0bb07d7373fa74e47c648b4d4e3b5950
Rosyid <rosyidgrobogan@gmail.com>
2021-01-12 21:38:11 +0700

    repository: add()

--- src/repository/TodoListRepositoryImpl.java
+++ src/repository/TodoListRepositoryImpl.java
@@ -13,11 +13,43 @@ public class TodoListRepositoryImpl implements TodoListRepository{
 
     @Override
     public void add(TodoList todoList) {
+        resizeIfFull();
 
+        for (int i = 0; i < data.length; i++){
+            if (data[i] == null){
+                data[i] = todoList;
+                break;
+            }
+        }
     }
 
     @Override
     public void remove(Integer number) {
 
+    }
+
+    public boolean isFull(){
+
+        boolean isFull = true;
+        for (int i = 0; i < data.length; i++){
+            if (data[i] == null){
+                // index masih ada yang kosong
+                isFull = false;
+                break;
+            }
+        }
+
+        return isFull;
+    }
+
+    public void resizeIfFull(){
+        if (isFull()){
+            TodoList[] temp = data;
+            data = new TodoList[data.length * 2];
+
+            for (int i = 0; i < temp.length; i++){
+                data[i] = temp[i];
+            }
+        }
     }
 }

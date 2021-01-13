66dc0cfac26a0c131f542fe7cf67c9027e45a808
Rosyid <rosyidgrobogan@gmail.com>
2021-01-13 07:26:06 +0700

    +viewTest: showTodoList()

---
+++ src/test/view/TodoListViewTest.java
@@ -0,0 +1,25 @@
+package test.view;
+
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+import view.TodoListView;
+
+public class TodoListViewTest {
+    public static void main(String[] args) {
+        testShowTodoList();
+    }
+
+    public static void testShowTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Olah Raga");
+        todoListService.addTodoList("Sarapan");
+        todoListService.addTodoList("Kerja");
+
+        todoListView.showTodoList();
+    }
+}

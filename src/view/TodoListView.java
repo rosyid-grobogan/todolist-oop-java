d4463153b358eaaaf6458f95cbe23d71e96e753f
Rosyid <rosyidgrobogan@gmail.com>
2021-01-13 07:19:16 +0700

    +view: showTodoList()

--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -1,8 +1,38 @@
 package view;
 
+import service.TodoListService;
+import util.InputUtil;
+
 public class TodoListView {
 
-    public void showTodoList(){}
+    private TodoListService todoListService;
+
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
+
+    public void showTodoList(){
+        while (true){
+            todoListService.showTodoList();
+
+            System.out.println("#MENU :");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+
+            String input = InputUtil.input("Pilih ");
+
+            if (input.equals("1")){
+                addTodoList();
+            } else if (input.equals("2")){
+                deleteTodoList();
+            }else if (input.equals("x")){
+                break;
+            }else {
+                System.out.println("Silakan pilih menu yang sudah disediakan");
+            }
+        }
+    }
     public void addTodoList(){}
     public void deleteTodoList(){}
 }
